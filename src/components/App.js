db1365879717b96e33af89c4d5a9928d35016c7f
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-23 16:40:34 -0800

    Add the reddit post component to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  const name = "Thato";
+  const name2 = "Haley";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
   );
 }
 
