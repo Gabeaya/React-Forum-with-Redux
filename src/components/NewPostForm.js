9653334f45c64d06e1d1fe590539b6d2ccbcc671
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 14:59:56 -0800

    Add a function for handling form submission.

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { v4 } from 'uuid'; 
 
 function NewPostForm(props){
+  
+  function handleNewPostFormSubmission(event){
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewPostFormSubmission}>
+        <input
+          type='text'
+          name='title'
+          placeholder='Pair Names' />
+        <textarea
+          type='text'
+          name='submission'
+          placeholder='Location' />
+        
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
