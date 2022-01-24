e09c65db5301f2ff34c9a45b3d5b2e793dec2125
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 11:51:21 -0800

    Add a construcror and a render for the view control.

---
+++ src/components/ToggleControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewPostForm from './NewPostForm';
+import RedditPost from './RedditPost';
+
+class ToggleControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default ToggleControl;
