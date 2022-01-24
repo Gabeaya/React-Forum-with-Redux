f94114712bbba8b221947fc5591087ae0206dfce
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 15:11:38 -0800

    Add proptypes for the post list created.

--- src/components/RedditPost.js
+++ src/components/RedditPost.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Post from "./Post";
+import PropTypes from "prop-types";
 
-function RedditPost() {
+function RedditPost(props) {
   return(
-    <Post/>
+    <React.Fragment>
+      <hr />
+      {props.postList.map((post, index) =>
+        <Post title={post.title}
+          submission={post.submission}
+          key={index} />
+        )}
+    </React.Fragment>
   );
 }
-
+PostList.propTypes = {
+  postList:PropTypes.array
+};
 export default RedditPost;
