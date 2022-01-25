9bdc31aa534414b1848ccd43381b867ac837d2b4
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 19:24:42 -0800

    Hard code some post info.

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,6 +1,14 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+
+  let action;
+  const postData = {
+    title: 'Daddy',
+    submission: "I'm going to be a dad",
+    vote: 5,
+    id: 1
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
