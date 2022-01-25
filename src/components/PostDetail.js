2214c1d77a2217c776414d914d6f6d2b6da32f22
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 16:17:56 -0800

    Add post props for details.

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function PostDetail(props){
+  const { post }= props;
   return(
     <React.Fragment>
       <h1>Details</h1>
+      <h3>{post.title}</h3>
+      <p>{post.submission}</p>
       <hr />
     </React.Fragment>
     
   );
 }
+
+PostDetail.propTypes = {
+  post: PropTypes.object
+};
+
 export default PostDetail;
