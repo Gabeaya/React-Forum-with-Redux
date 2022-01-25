b3a5a4f35873a187379f17d2840966418fe64fb5
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-24 19:42:19 -0800

    Write a test for adding posts.

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -5,11 +5,33 @@ describe('postListReducer', () => {
   let action;
   const postData = {
     title: 'Daddy',
-    submission: "I'm going to be a dad",
+    submission: "I",
     vote: 5,
     id: 1
-  }
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new post data to mainPostList', () => {
+    const { title, submission, vote, id } = postData;
+    action = {
+      type: 'ADD_POST',
+      title: title,
+      submission: submission,
+      vote: vote,
+      id: id
+    };
+
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        title: title,
+        submission: submission,
+        vote: vote,
+        id: id
+      }
+    });
+  });
+
 });
