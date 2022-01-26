d9f827510ee65543b0d3a15cab8fc00655925b7e
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-25 20:11:27 -0800

    Change the state to the object literal.

--- src/components/ToggleControl.js
+++ src/components/ToggleControl.js
@@ -43,7 +43,7 @@ class ToggleControl extends React.Component {
   }
 
   handleChangingSelectedPost = (id) => {
-    const selectedPost = this.state.mainPostList.filter(post => post.id === id)[0];
+    const selectedPost = this.props.mainPostList[id];
     this.setState({selectedPost: selectedPost});
   }
 
