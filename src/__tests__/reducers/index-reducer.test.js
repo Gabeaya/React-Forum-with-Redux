115f7d6e18d3b376937a63b83465dd488505110e
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-26 19:31:04 -0800

    Test that our initial state of post list matches the root reducer.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,12 +1,24 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import postListReducer from '../../reducers/post-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      mainPropList: {},
+      mainPostList: {},
       formVisibleOnPage: false
     });
   });
 
+  test('Check that initial state of post list reducer matches root reducer', () => {
+    expect(store.getState().mainPostList).toEqual(postListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 });
